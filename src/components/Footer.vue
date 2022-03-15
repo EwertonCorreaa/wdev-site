19cf48bfc788650fde2c3159d66a6280c7f407bf
Ewerton Corrêa <98770616+EwertonCorreaa@users.noreply.github.com>
2022-03-15 15:19:39 -0300

    Adição de novos arquivos

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'footeR'
+}
+</script>
+
+<style scoped>
+footer{
+    height: 60px;
+    width: 100%;
+    background-color: var(--color-background-nav);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <main>
+        <div class="text-top">Vídeos novos toda <b>quinta</b> as <b>20h</b></div>
+        <a href="https://youtube.com/wdevoficial" target="blank"><img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/subscribe.svg
+" alt="Inscreva-se" id="subscribe"></a>
+        <div class="text-bottom">youtube.com/wdevoficial</div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'homE',
+    
+}
+</script>
+
+<style scoped>
+
+main{
+    background-color: var(--color-background-home);
+    justify-content: center;
+    align-items: center;
+}
+
+.text-top{
+    color: var(--color-text-light);
+    font-size: 22px;
+    margin-bottom: 20px;
+}
+
+.text-bottom{
+    color: var(--color-text-light);
+    font-size: 18px;
+    margin-bottom: 20px;
+}
+
+#subcribe{
+    width: 340px;
+}
+
+@media (min-width:700px){
+    #subcribe{
+        width: 370px;
+    }
+}
+
+</style>
